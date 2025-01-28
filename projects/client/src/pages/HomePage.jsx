f36b436ee7e4c80712ce55aaef2876f9c0dc05b1
novrialsandi <novrialsandi@gmail.com>
2025-01-28 10:54:31 +0700

    setup

--- projects/client/src/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-import { Center } from "@chakra-ui/react";
-
-export default function Homepage() {
-	return <Center>hello</Center>;
-}
--- projects/client/src/routes/Routes.js
+++
@@ -1,6 +0,0 @@
-import { Route } from "react-router-dom";
-import Homepage from "../pages/HomePage";
-
-const routes = [<Route path="/" element={<Homepage />}></Route>];
-
-export default routes;
