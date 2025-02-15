3b111353bde6d11cf1453e2129f84e520788af96
novrialsandi <novrialsandi@gmail.com>
2025-02-16 01:33:33 +0700

    chore: initialize project structure and add configuration files

---
+++ client/src/app/(Users)/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const div = () => {
+	return (
+		<main>
+			<div>haha</div>
+		</main>
+	);
+};
+
+export default div;
---
+++ server/src/index.js
@@ -0,0 +1,64 @@
+const { join } = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: join(__dirname, "../.env") });
+const express = require("express");
+const cors = require("cors");
+const router = require("./routers");
+const db = require("./models");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+//#region API ROUTES
+
+// ===========================
+// NOTE : Add your routes here
+
+app.get("/api", (req, res) => {
+	res.send(`Hello, this is my API`);
+});
+
+// ===========================
+
+// not found
+app.use((req, res, next) => {
+	if (req.path.includes("/api/")) {
+		res.status(404).send("Not found !");
+	} else {
+		next();
+	}
+});
+
+// error
+app.use((err, req, res, next) => {
+	if (req.path.includes("/api/")) {
+		console.error("Error : ", err.stack);
+		res.status(500).send("Error !");
+	} else {
+		next();
+	}
+});
+
+//#endregion
+
+//#region CLIENT
+const clientPath = "../../client/build";
+app.use(express.static(join(__dirname, clientPath)));
+
+// Serve the HTML page
+app.get("*", (req, res) => {
+	res.sendFile(join(__dirname, clientPath, "index.html"));
+});
+
+//#endregion
+
+app.listen(PORT, (err) => {
+	if (err) {
+		console.log(`ERROR: ${err}`);
+	} else {
+		console.log(`APP RUNNING at ${PORT} ✅`);
+	}
+});
