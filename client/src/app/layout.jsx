756271016d3b109da05f64c2946a8f7405edc449
Novrial Sandi <122582669+novrialsandi@users.noreply.github.com>
2025-02-21 14:40:10 +0700

    feat: integrate Single Day font into layout for improved typography

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import { Single_Day } from "next/font/google";
+
+const singleDay = Single_Day({
+	weight: "400", // Single Day is only available in 400 weight
+	subsets: ["latin"], // Specify the subset you need
+	display: "swap",
+});
 
 export const metadata = {
 	title: "Yomi's Project",
@@ -9,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={`min-h-svh flex items-center justify-center`}>
+			<body
+				className={`${singleDay.className} min-h-svh flex items-center justify-center`}
+			>
 				{children}
 			</body>
 		</html>
