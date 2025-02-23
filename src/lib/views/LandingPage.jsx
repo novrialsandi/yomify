c41be4aa996c6b7199d26008d2c299aa734765c8
Novrial Sandi <122582669+novrialsandi@users.noreply.github.com>
2025-02-23 11:42:32 +0700

    feat: update metadata for MainPage and demo page, implement LandingPage component, and enhance layout structure

--- src/lib/views/LandingPage.jsx
+++ src/lib/views/LandingPage.jsx
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LandingPage = () => {
+	const router = useRouter();
+
 	return (
-		<div>
-			<div></div>
+		<div className="min-h-svh bg-[#EEE9D2] flex items-center p-4">
+			<img
+				src="/meta.png"
+				alt="Meta"
+				onClick={() => router.push("/demo")}
+				className="cursor-pointer"
+			/>
 		</div>
 	);
 };
