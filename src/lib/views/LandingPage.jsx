2776890900e392c8f593457a82fd564026fa9a4c
novrialsandi <novrialsandi@gmail.com>
2025-02-23 16:57:08 +0700

    feat: refine LandingPage layout by adjusting spacing and reintroducing demo button

--- src/lib/views/LandingPage.jsx
+++ src/lib/views/LandingPage.jsx
@@ -37,7 +37,7 @@ const LandingPage = () => {
 
 	return (
 		<>
-			<div className="min-h-svh bg-[#EEE9D2] flex flex-col gap-8 justify-center items-center p-4">
+			<div className="min-h-svh bg-[#EEE9D2] flex flex-col gap-6 justify-center items-center p-4">
 				<div className="text-center text-7xl font-bold ">Yomify</div>
 				<img src="/meta.png" alt="Meta" className="cursor-pointer size-[80%]" />
 				<div className="text-center text-4xl font-bold ">
@@ -47,6 +47,12 @@ const LandingPage = () => {
 					Engage your audience with clickable visuals that reveal event details,
 					stories, and more.
 				</p>
+				<Button
+					onClick={() => router.push("/demo")}
+					class="squishy squishy-candy"
+				>
+					Try Demo
+				</Button>
 			</div>
 			<div className="min-h-lvh bg-[#9AC0AF] flex flex-col gap-8 justify-center items-center p-8">
 				<h2 className="text-3xl font-semibold text-center">
@@ -68,12 +74,6 @@ const LandingPage = () => {
 						))}
 					</Carousel>
 				</div>
-				<Button
-					onClick={() => router.push("/demo")}
-					class="squishy squishy-candy"
-				>
-					Try Demo
-				</Button>
 			</div>
 			<div className="bg-[#EEE9D2] w-full flex flex-col justify-center items-center p-4">
 				<h2 className="text-3xl font-semibold text-center ">
