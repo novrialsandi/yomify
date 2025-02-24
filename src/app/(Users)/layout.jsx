29100d48c1502fd3fdbdf6d10f23d72bc8e0f86d
novrialsandi <novrialsandi@gmail.com>
2025-02-24 09:23:45 +0700

    feat: update UserLayout to display current version from package.json

--- src/app/(Users)/layout.jsx
+++ src/app/(Users)/layout.jsx
@@ -1,3 +1,18 @@
-export default function UserLayout({ children }) {
-	return <div className="max-w-[544px] w-full overflow-hidden">{children}</div>;
-}
+import pgJson from "../../../package.json";
+
+import React from "react";
+
+const UserLayout = ({ children }) => {
+	return (
+		<div className="max-w-[544px] w-full relative overflow-hidden">
+			{children}
+			<div className="relative flex w-full justify-end">
+				<div className="fixed bottom-0 text-xs px-1 bg-white rounded-tl-md">
+					v{pgJson.version}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UserLayout;
