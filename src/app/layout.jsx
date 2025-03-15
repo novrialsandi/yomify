37b5f9220a5bc62e4c67907b729deee1e8f3bf12
novrialsandi <novrialsandi@gmail.com>
2025-03-16 00:04:32 +0700

    feat: bump version to 0.4.7 and add viewport meta tag to layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,12 @@ const singleDay = Single_Day({
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" translate="no">
+			<head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1"
+				/>
+			</head>
 			<body
 				className={`${singleDay.className} min-h-svh flex items-center justify-center`}
 			>
